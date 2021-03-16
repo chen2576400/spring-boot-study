4f15dbd34f9cc0e3f0cf3ace5ea05de31ead03dd
nchen <2576400553@qq.com>
2021-03-16 10:22:37 +0800

    枚举示例  反射添加

--- src/main/java/com/chenning/springbootlearn/reflex/model/service/BaseService.java
+++ src/main/java/com/chenning/springbootlearn/reflex/model/service/BaseService.java
@@ -2,10 +2,12 @@ package com.chenning.springbootlearn.reflex.model.service;
 
 import com.chenning.springbootlearn.demonBuild.model.User;
 import com.chenning.springbootlearn.demonBuild.service.UserService;
+import com.chenning.springbootlearn.reflex.model.AgentEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * @author: Mr.Chen
@@ -13,20 +15,49 @@ import java.util.List;
  **/
 @Service
 public class BaseService {
+    private LinkedBlockingQueue<AgentEntity> queue = new LinkedBlockingQueue<AgentEntity>(4096);
+
     @Autowired
     UserService userService;
 
     public void x(User user) {
         List<User> allUser = userService.findAllUser();
-        System.out.println(("用户ID"+user.getUserId() +"用户名字"+user.getUserName()+"所有用户数量")+allUser==null?0:allUser.size());
+        System.out.println(("用户ID" + user.getUserId() + "用户名字" + user.getUserName() + "所有用户数量") + allUser == null ? 0 : allUser.size());
     }
 
-    public void doSomeThingByDance(User user){
-        System.out.println("DanceService===============>"+"参数用户"+user.getUserName());
+    public void doSomeThingByDance(User user) {
+        System.out.println("DanceService===============>" + "参数用户" + user.getUserName());
     }
 
 
-    public void doSomeThingBySing(String test,User user){
-        System.out.println("SingService==========>"+test+"参数用户"+user.getUserName());
+    public void doSomeThingBySing(String test, User user) {
+        System.out.println("SingService==========>" + test + "参数用户" + user.getUserName());
     }
+
+
+
+    public synchronized void add(AgentEntity entity) {
+        try {
+            queue.put(entity);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void start() {
+        new Thread(() -> {
+
+            while (true) {
+                try {
+                    AgentEntity entity = queue.take();
+                    entity.getMethod().invoke(entity.getObject(), entity.getArgs());
+                    Thread.sleep(4000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
 }
