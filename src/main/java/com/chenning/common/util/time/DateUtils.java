7f4c0ea83840b704779a287de2489c24f4cb6ab2
chenning <2576400553@qq.com>
2022-04-20 18:06:46 +0800

    时间工具类更新

--- src/main/java/com/chenning/common/util/time/DateUtils.java
+++ src/main/java/com/chenning/common/util/time/DateUtils.java
@@ -7,6 +7,7 @@ import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -68,7 +69,6 @@ public class DateUtils {
 
     /**
      * 解析字符串日期为Date
-     *
      * @param dateStr 日期字符串
      * @param pattern 格式
      * @return Date
@@ -88,11 +88,8 @@ public class DateUtils {
      * @return 日期字符串
      */
     public static String format(Date date, String pattern) {
-
         Instant instant = date.toInstant();
-
         LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
-
         return localDateTime.format(DateTimeFormatter.ofPattern(pattern));
     }
 
@@ -110,7 +107,6 @@ public class DateUtils {
         return Date.from(zonedDateTime.toInstant());
     }
 
-
     /**
      * LocalDateTime类型转为Date
      *
@@ -121,44 +117,58 @@ public class DateUtils {
         return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
     }
 
-
     /**
-     * @return 返回当天的起始时间
+     * 时间戳转  localDateTime
+     *
+     * @param  milliseconds 时间戳
+     * @return
      */
-    public static Date getStartTime() {
-
-        LocalDateTime now = LocalDateTime.now().withHour(0).withMinute(0).withSecond(0);
-        return localDateTime2Date(now);
+    public static LocalDateTime localDateTime(Long milliseconds) {
+        Instant instant = Instant.ofEpochMilli(milliseconds);
+        return LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
     }
 
 
     /**
-     * @return 返回当天的结束时间
+     * 时间戳转 LocalDate
+     *
+     * @param milliseconds 时间戳
+     * @return
      */
-    public static Date getEndTime() {
-        LocalDateTime now = LocalDateTime.now().withHour(23).withMinute(59).withSecond(59).withNano(999);
-        return localDateTime2Date(now);
+    public static LocalDate millisecondsToLocalDate(Long milliseconds) {
+        return Instant.ofEpochMilli(milliseconds).atZone(ZoneOffset.ofHours(8)).toLocalDate();
     }
 
 
     /**
-     * @param startTime
-     * @param endTime
-     * @return 以小时为单位。如：2018-08-08 和 2018-08-07 相差24h
+     * 返回当天的起始时间  为空默认当天
+     *
+     * @param milliseconds 指定时间戳
+     * @return
      */
-    public static double getDistanceTime(Date startTime, Date endTime) {
-        double hour = 0;
-        long time1 = startTime.getTime();
-        long time2 = endTime.getTime();
-
-        long diff;
-        if (time1 < time2) {
-            diff = time2 - time1;
-        } else {
-            diff = time1 - time2;
+    public static Date getStartTime(Long milliseconds) {
+        LocalDateTime now;
+        if (milliseconds == null) {
+            now = LocalDateTime.now().withHour(0).withMinute(0).withSecond(0);
         }
-        hour = (diff / (60 * 60 * 1000));
-        return hour;
+        now = localDateTime(milliseconds).withHour(0).withMinute(0).withSecond(0);
+        return localDateTime2Date(now);
+    }
+
+
+    /**
+     * 返回当天的结束时间  为空默认当天
+     *
+     * @param milliseconds 指定时间戳
+     * @return
+     */
+    public static Date getEndTime(Long milliseconds) {
+        LocalDateTime now;
+        if (milliseconds == null) {
+            now = LocalDateTime.now().withHour(23).withMinute(59).withSecond(59).withNano(999);
+        }
+        now = localDateTime(milliseconds).withHour(23).withMinute(59).withSecond(59).withNano(999);
+        return localDateTime2Date(now);
     }
 
 
@@ -205,7 +215,7 @@ public class DateUtils {
 
     /**
      * +推迟   -提前
-     *
+     * <p>
      * 日期加减，提供当前日期以及指定的年月日时分秒进行日期加减；
      *
      * @param now    当前数字格式日期
@@ -235,6 +245,60 @@ public class DateUtils {
             cal.add(Calendar.YEAR, year);
 
         return cal.getTime().getTime();
+    }
+
+    /**
+     * @param year  2021
+     * @param month 8
+     * @return 指定年月的开始时间   2021-08-01 00:00:00
+     */
+    public static Date getBeginTime(int year, int month) {
+        YearMonth yearMonth = YearMonth.of(year, month);
+        LocalDate localDate = yearMonth.atDay(1);
+        LocalDateTime startOfDay = localDate.atStartOfDay();
+        ZonedDateTime zonedDateTime = startOfDay.atZone(ZoneId.of("Asia/Shanghai"));
+
+        return Date.from(zonedDateTime.toInstant());
+    }
+
+    /**
+     * @param year  2021
+     * @param month 8
+     * @return 指定年月的结束时间   2021-08-31 23:59:59
+     */
+    public static Date getEndTime(int year, int month) {
+        YearMonth yearMonth = YearMonth.of(year, month);
+        LocalDate endOfMonth = yearMonth.atEndOfMonth();
+        LocalDateTime localDateTime = endOfMonth.atTime(23, 59, 59, 999);
+        ZonedDateTime zonedDateTime = localDateTime.atZone(ZoneId.of("Asia/Shanghai"));
+        return Date.from(zonedDateTime.toInstant());
+    }
+
+
+    /**
+     * @param startMilliseconds 开始时间戳
+     * @param endMilliseconds   结束时间戳
+     * @param chronoUnit        "相差的年数ChronoUnit.YEARS
+     *                          "相差的月数:ChronoUnit.MONTHS
+     *                          "相差的周数:ChronoUnit.WEEKS
+     *                          "相差的天数:ChronoUnit.DAYS
+     *                          "相差的时数:ChronoUnit.HOURS
+     *                          "相差的分数:ChronoUnit.MINUTES
+     *                          "相差的秒数:ChronoUnit.SECONDS
+     *                          "相差的毫秒数:ChronoUnit.MILLIS
+     *                          "相差的微秒数:ChronoUnit.MICROS
+     *                          "相差的纳秒数:ChronoUnit.NANOS.between(birthDate, today));
+     *                          "相差的半天数:ChronoUnit.HALF_DAYS.between(birthDate, today));
+     *                          "相差的十年数:ChronoUnit.DECADES
+     *                          "相差的世纪有年数:ChronoUnit.CENTURIES
+     *                          "相差的千年数:ChronoUnit.MILLENNIA
+     *                          "相差的纪元数:ChronoUnit.ERAS
+     * @return long
+     */
+    public static Long calculateTimeDifference(Long startMilliseconds, Long endMilliseconds, ChronoUnit chronoUnit) {
+        LocalDate startLocalDate = millisecondsToLocalDate(startMilliseconds);
+        LocalDate endLocalDate = millisecondsToLocalDate(endMilliseconds);
+        return chronoUnit.between(startLocalDate, endLocalDate);
     }
 
 
@@ -279,6 +343,5 @@ public class DateUtils {
         return null;
 
     }
-
 
 }
